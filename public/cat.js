75030a73050570233228c96bbfc27e268be351bf
brtva1997 <bretatvardek@gmail.com>
2025-06-18 10:18:33 +0100

    asd

---
+++ public/cat.js
@@ -0,0 +1,17 @@
+const cat = document.getElementById('cat');
+let clickCount = 0;
+
+cat.addEventListener('click', () => {
+  clickCount++;
+  if (clickCount === 1) {
+    cat.classList.add('lick');
+    setTimeout(() => cat.classList.remove('lick'), 1200);
+  } else if (clickCount === 2) {
+    cat.src = 'cat-smile.svg';
+    cat.classList.add('smile');
+    localStorage.setItem('catClicked', 'true');
+    setTimeout(() => {
+      window.location.href = 'index.html';
+    }, 1200);
+  }
+});
