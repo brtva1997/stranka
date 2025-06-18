4e42d79564071501207d666e9acfa7d9fafbf388
brtva1997 <bretatvardek@gmail.com>
2025-06-18 10:25:05 +0100

    asd

--- public/cat.js
+++ public/cat.js
@@ -7,7 +7,7 @@ cat.addEventListener('click', () => {
     cat.classList.add('lick');
     setTimeout(() => cat.classList.remove('lick'), 1200);
   } else if (clickCount === 2) {
-    cat.src = 'cat-smile.svg';
+    cat.src = 'cat2.png';
     cat.classList.add('smile');
     localStorage.setItem('catClicked', 'true');
     setTimeout(() => {
